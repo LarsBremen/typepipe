f1b90dd4cde753c7ca861521588da1625d79ad50
LarsBremen <lars@paucity.dev>
2022-08-22 15:31:26 +0200

    add test with the example

--- spec/test.spec.ts
+++ spec/test.spec.ts
@@ -1,7 +1,17 @@
 import {expect} from 'chai';
+import {pipe} from '../src/pipe';
 
-describe('placeholder test', () => {
-  it('placeholder test', () => {
-    expect(1).equal(1);
+describe('Test pipe function', () => {
+  it('Should be {t: \'12\'}', () => {
+    const add2 = (x: number): number => x + 2;
+    const nrToStr = (x: number): string => x.toString();
+    const strToObj = (x: string): {t: string} => ({t: x});
+
+    const add2ToStrToObj = pipe(
+        add2,
+        nrToStr,
+        strToObj,
+    );
+    expect('12').equal(add2ToStrToObj(10).t);
   });
 });
