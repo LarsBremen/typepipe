3d17206cfe1b856b258e2cf08385093d62be9aa9
LarsBremen <lars@paucity.dev>
2022-08-22 15:20:11 +0200

    enhance js doc

--- src/pipe.ts
+++ src/pipe.ts
@@ -21,6 +21,22 @@ export function pipe<A, B, C, T>(
  * one parameter. The given parameter is passed to the first function and
  * the result of this is passed on to the next function. This gets repeated
  * until the result of the last function is returned.
+ *
+ * @return {Function} that executes the functions passed to pipe
+ *
+ * @example
+ * ```ts
+ * const add2 = (x: number): number => x + 2;
+ * const nrToStr = (x: number): String => x.toString();
+ *
+ * const add2ToStr = pipe(
+ *  add2,
+ *  nrToStr,
+ * )
+ *
+ * console.log(add2ToStr(10);
+ * // -> '12'
+ * ```
  */
 export function pipe(...fns: opFunction<any, any>[]): Res<any> {
   return (y: any) => fns.reduce((x, f) => f(x), y);
